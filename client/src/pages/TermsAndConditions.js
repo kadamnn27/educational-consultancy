c54a5321568d211947bda7bbb701e71742d0710b
kadamnn27 <166747977+kadamnn27@users.noreply.github.com>
2024-04-17 08:00:59 +0530

    Update TermsAndConditions.js

--- client/src/pages/TermsAndConditions.js
+++ client/src/pages/TermsAndConditions.js
@@ -1,64 +1,84 @@
 import React from 'react';
+import { Typography, Container } from '@material-ui/core';
 
 const TermsAndConditions = () => {
     return (
-        <div>
-            <h1>Terms and Conditions</h1>
-            <h3>Terms and Conditions</h3>
-            <h4>Introduction</h4>
-            <p>Please read these Terms of Use (“Terms”), a legal agreement between Expert Educational Consultancy, a proprietorship concern of Mr. Shamsher Singh Rana having PAN: ADPPR9160A and GST No. 07ADPPR9160A2ZB and having place of business at 104 Aditya Complex-2 D Block Central Market Prashant Vihar Sec-14 Rohini, Delhi (Hereinafter “Expert Educational Consultancy ”/ “EEC”/ “We”)</p>
-            <p>AND</p>
-            <p>Those people who use this Website (defined below) of the second part (hereinafter collectively referred to as “you”, “yours” or “User”).</p>
+        <Container style={{ margin: "80px auto" }}>
+            <Typography align="center" variant="h1">
+                Terms and Conditions
+            </Typography>
+            <Typography variant="h3">Terms and Conditions</Typography>
+            <Typography variant="h4">Introduction</Typography>
+            <Typography>
+                Please read these Terms of Use (“Terms”), a legal agreement between Expert Educational Consultancy, a proprietorship concern of Mr. Shamsher Singh Rana having PAN: ADPPR9160A and GST No. 07ADPPR9160A2ZB and having place of business at 104 Aditya Complex-2 D Block Central Market Prashant Vihar Sec-14 Rohini, Delhi (Hereinafter “Expert Educational Consultancy ”/ “EEC”/ “We”)
+            </Typography>
+            <Typography>
+                AND
+            </Typography>
+            <Typography>
+                Those people who use this Website (defined below) of the second part (hereinafter collectively referred to as “you”, “yours” or “User”).
+            </Typography>
             
-            <h4>Use of the website by Users</h4>
-            <p>Price: Expert Educational Consultancy, however, reserves its rights to charge a fee and the offerings therein in the future.</p>
+            <Typography variant="h4">Use of the website by Users</Typography>
+            <Typography>
+                Price: Expert Educational Consultancy, however, reserves its rights to charge a fee and the offerings therein in the future.
+            </Typography>
             
-            <h4>Termination /Modification/Suspension:</h4>
-            <p>The EEC may at any time, terminate its legal agreement with you if you have breached any provision of the terms (or have acted in manner which clearly shows that you do not intend to, or are unable to comply with the provisions of the terms).</p>
+            <Typography variant="h4">Termination /Modification/Suspension:</Typography>
+            <Typography>
+                The EEC may at any time, terminate its legal agreement with you if you have breached any provision of the terms (or have acted in manner which clearly shows that you do not intend to, or are unable to comply with the provisions of the terms).
+            </Typography>
             
-            <p>We may update or change the Website and/or Information provided therein from time to time and recommend that you review these terms on a regular basis. You understand and agree that your continued use of the Website after the terms have been updated or changed constitutes your acceptance of the revised terms.</p>
+            <Typography variant="h4">Cancellation, return & Refund policy:</Typography>
+            <Typography>
+                Fees once paid are not refundable.
+            </Typography>
             
-            <h4>Cancellation, return & Refund policy:</h4>
-            <p>Fees once paid are not refundable.</p>
+            <Typography variant="h4">Governing Law and Jurisdiction</Typography>
+            <Typography>
+                The interpretation of this Agreement and the resolution of any disputes arising under this Agreement shall be governed by the laws of India and the courts at Delhi shall have jurisdiction, subject to the Arbitration clause below.
+            </Typography>
             
-            <h4>Governing Law and Jurisdiction</h4>
-            <p>The interpretation of this Agreement and the resolution of any disputes arising under this Agreement shall be governed by the laws of India and the courts at Delhi shall have jurisdiction, subject to the Arbitration clause below.</p>
+            <Typography variant="h4">Arbitration:</Typography>
+            <Typography>
+                1) Any dispute, claim or controversy arising out of or relating to this Agreement or the breach, termination, enforcement, interpretation or validity thereof, including the determination of the scope or applicability of this Agreement to arbitrate, or to your use of the Website or the information to which it gives access, shall be determined by Arbitration in India, before a single arbitrator in accordance with the Arbitration and Conciliation Act, 1996 along with all amendments. The venue of such arbitration shall be Delhi, India. The governing law of the Agreement shall be the substantive law of India.
+            </Typography>
             
-            <h4>Arbitration:</h4>
-            <p>1) Any dispute, claim or controversy arising out of or relating to this Agreement or the breach, termination, enforcement, interpretation or validity thereof, including the determination of the scope or applicability of this Agreement to arbitrate, or to your use of the Website or the information to which it gives access, shall be determined by Arbitration in India, before a single arbitrator in accordance with the Arbitration and Conciliation Act, 1996 along with all amendments. The venue of such arbitration shall be Delhi, India. The governing law of the Agreement shall be the substantive law of India.</p>
+            <Typography variant="h4">Amendments</Typography>
+            <Typography>
+                We may vary these terms and conditions. Any variations become effective on posting on the website. By making a payment through this website after the terms and conditions have been varied you agree to be bound by that variation.
+            </Typography>
             
-            <h4>Amendments</h4>
-            <p>We may vary these terms and conditions. Any variations become effective on posting on the website. By making a payment through this website after the terms and conditions have been varied you agree to be bound by that variation.</p>
+            <Typography variant="h4">Security</Typography>
+            <Typography>
+                We take reasonable security measures and procedures, and as specified by applicable law, to maintain appropriate physical, technical, and administrative security to help prevent the loss, misuse, or unauthorized access, disclosure or modification of Personal Information.
+            </Typography>
             
-            <h4>Security</h4>
-            <p>We take reasonable security measures and procedures, and as specified by applicable law, to maintain appropriate physical, technical, and administrative security to help prevent the loss, misuse, or unauthorized access, disclosure or modification of Personal Information.</p>
+            <Typography variant="h4">Disclaimer and Exclusion of Warranties</Typography>
+            <Typography>
+                The website and the information contained therein are provided on an “as is” and “as available” basis without any warranty of any kind, expressed or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringement.
+            </Typography>
             
-            <h4>Severability</h4>
-            <p>If any provision of these terms and conditions are held to be invalid, unlawful void or otherwise unenforceable, then that provision will be deemed severed from these terms and conditions and will not affect the validity and enforceability of any remaining provisions.</p>
+            <Typography variant="h4">Limitation of Liability</Typography>
+            <Typography>
+                In no event shall the EEC, or its respective officers, managers, members, directors, employees, successors, assigns, subsidiaries, affiliates, suppliers, attorneys or agents, be liable to you for any direct, indirect, incidental, special, punitive, consequential or exemplary damages whatsoever resulting from any
+            </Typography>
             
-            <h4>Use of information</h4>
-            <p>The materials and information provided on this website are for reference purposes only.</p>
-            
-            <h4>Other Websites:</h4>
-            <p>Expert Educational Consultancy may contain links to other websites. Please note that when you click on one of these links, you are entering another website/application over which we have no control and will bear no responsibility.</p>
-            
-            <h4>Make no action or omission.</h4>
-            <p>Anyone using this website should not take any action and/or omit to take any action based in whole or in part on information found on this website without first seeking the advice of the actual organisation.</p>
-            
-            <h4>Disclaimer and Exclusion of Warranties</h4>
-            <p>The website and the information contained therein are provided on an “as is” and “as available” basis without any warranty of any kind, expressed or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringement.</p>
-            
-            <h4>Limitation of Liability</h4>
-            <p>In no event shall the EEC, or its respective officers, managers, members, directors, employees, successors, assigns, subsidiaries, affiliates, suppliers, attorneys or agents, be liable to you for any direct, indirect, incidental, special, punitive, consequential or exemplary damages whatsoever resulting from any</p>
-            
-            <h4>Grievance officer</h4>
-            <p>In accordance with Information Technology Act 2008 and rules made thereunder, the name and contact details of the Grievance Officer is as follows:</p>
-            <p>Name: Shamsher Singh Rana</p>
-            <p>Email Id: expertec303@yahoo.co.in</p>
-            <p>Last updated 04 April 2024.</p>
-            <p>© 1995 Expert Educational Consultancy. All Rights Reserved</p>
-        </div>
+            <Typography variant="h4">Grievance officer</Typography>
+            <Typography>
+                In accordance with Information Technology Act 2008 and rules made thereunder, the name and contact details of the Grievance Officer is as follows:
+                <br />
+                Name: Shamsher Singh Rana
+                <br />
+                Email Id: expertec303@yahoo.co.in
+                <br />
+                Last updated 04 April 2024.
+                <br />
+                © 1995 Expert Educational Consultancy. All Rights Reserved
+            </Typography>
+        </Container>
     );
 }
 
 export default TermsAndConditions;
+
